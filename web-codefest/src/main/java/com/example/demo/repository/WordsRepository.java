a3a778e65160602a709a90fede6aaaa3681ab3c2
Malinga Hashan Wiswajith Ekanayaka <40067206+MHw19@users.noreply.github.com>
2021-04-06 19:08:42 +0530

    related to word entity
    complete add  ,delete , update

--- web-codefest/src/main/java/com/example/demo/repository/WordsRepository.java
+++ web-codefest/src/main/java/com/example/demo/repository/WordsRepository.java
@@ -1,5 +1,18 @@
 package com.example.demo.repository;
 
-public interface WordsRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.Words;
+
+@Repository
+public interface WordsRepository  extends JpaRepository<Words,Integer>{
+	
+	
+	
+	@Query("SELECT u From Words u WHERE u.keyword=?1")
+	public Words getserachByKeyword(String keyword);
+	
 
 }
--- web-codefest/src/main/java/com/example/demo/service/wordsService.java
+++ web-codefest/src/main/java/com/example/demo/service/wordsService.java
@@ -1,5 +1,84 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Words;
+import com.example.demo.repository.WordsRepository;
+
+
+@Service
 public class wordsService {
+	
+	
+	@Autowired
+	WordsRepository wordsRepository;
+	
+	
+	public Words getserachByKeyword(String keyword) {
+		
+		
+		return wordsRepository.getserachByKeyword(keyword);
+		
+		
+	}
+	
+	
+	public Words addProduct(Words word) {
+		
+		
+		return wordsRepository.save(word);
+		
+	}
+	
+	
+	
+     public String deletewordst(int  id) {
+		
+    Words word	= wordsRepository.findById(id).orElse(null);
+    	 
+     String responses="";
+    
+		if(word !=null) {
+			
+			
+			wordsRepository.delete(word);
+			
+			responses=word.getKeyword()+" Deleted";
+			
+		}else {
+			
+			responses=" Not  Deleted";
+			
+			
+		}
+		
+		
+		
+		return responses;
+		
+	}
+     
+     public Words updateWords(Words word) {
+    	 
+         Words savewords = wordsRepository.findById(word.getId()).orElse(null);
+         if (savewords != null) {
+         	
+        	 savewords.setDefinition(word.getDefinition());
+        	 savewords.setKeyword(word.getKeyword());
+             
+             
+         }
+         return savewords;
+     }
+     
+     
+     
+	
+	
+	
+	
+	
+	
 
 }
