b231298733987917e76d35918cc42582d0463182
R-Yasasri <rajithayasssri@gmail.com>
2021-04-06 15:20:12 +0530

    Project sturcture
    Created the project structure including front end folder

---
+++ web-codefest/src/main/java/com/example/demo/WebCodefestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCodefestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCodefestApplication.class, args);
+	}
+
+}
